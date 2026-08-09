35846ba4ba16bdc732046a8fd95498ce7caedf8b
Laxmi Sah <laxmisah32145@gmail.com>
2026-08-09 14:25:51 +0545

    chore: remove temporary commit script

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+set GIT="C:\Program Files\Git\cmd\git.exe"
+%GIT% rm commit.bat
+%GIT% commit -m "chore: remove temporary commit script"
+%GIT% push origin master
--- commit.bat
+++
@@ -1,4 +0,0 @@
-set GIT="C:\Program Files\Git\cmd\git.exe"
-%GIT% add .
-%GIT% commit -m "fix: global refactor of model imports to central registry to prevent login 500 errors"
-%GIT% push origin master
